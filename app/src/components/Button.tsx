aee1f5dbb096ee3a7fca43ec3e4287ddbc7dcca9
smenaq <sergiosantosmenaq@gmail.com>
2023-04-10 19:05:47 -0500

    changing some styles in button

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -15,17 +15,31 @@ function Button(){
             const res = await classify(context.input);
             setWait(false)
             context?.setResult(res)
+        } else {
+            context?.setResult({topic:'Input no permitido', confidence: 0})
         }
     }
     
+    if(!wait)
     return (
-        <div className='flex w-full justify-center mt-6 '>
-            <button type="button" onClick={ handleButton } className="flex place-content-center gap-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                clasificar
+        <div className='flex w-full justify-center mt-6'>
+            <button type="button" onClick={ handleButton } className="flex items-center gap-3 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-90">
+                <p>Clasificar</p>
+            </button>
+        </div>
+    )
+
+    else 
+    return (
+        <div className='flex w-full justify-center mt-6'>
+            <button type="button" className="flex items-center gap-3 text-white bg-gray-500 hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-90">
+                <p>Clasificando</p>
                 {wait && <img src={loadingPng} className="w-7 animate-spin" />}
             </button>
         </div>
     )
+
+
 }
 
 export default Button
