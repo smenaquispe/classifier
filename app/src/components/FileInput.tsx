f057d219152ff7c1031c9e5abb1f8ccd788a8abb
smenaq <sergiosantosmenaq@gmail.com>
2023-04-13 17:32:25 -0500

    case of pdf done

--- app/src/components/FileInput.tsx
+++ app/src/components/FileInput.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import { Context } from "../providers/context"
 import { useContext } from "react"
+import * as pdfjsLib from 'pdfjs-dist';
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.5.141/pdf.worker.min.js';
 
 function FileInput(){
 
     const context = useContext(Context)
 
-    const read = (e : ProgressEvent<FileReader>) => {
+    const readTextFile = (e : ProgressEvent<FileReader>) => {
         const input = context?.refInput;
         if(input && e.target?.result?.toString()) {
             input!.current!.innerHTML = e.target?.result?.toString()
@@ -14,10 +17,36 @@ function FileInput(){
         }
     }
 
-    const handleFile = (e : React.ChangeEvent<HTMLInputElement>) => {
-        const file = new FileReader()
-        file.addEventListener('load', read)
-        file.readAsText(e!.target!.files![0])
+    const handleFile = async (e : React.ChangeEvent<HTMLInputElement>) => {
+        const extension = e.target.value.split('.')[1]
+
+        switch (extension) {
+            case 'txt':
+                const file = new FileReader()
+                file.addEventListener('load', readTextFile)
+                file.readAsText(e!.target!.files![0])        
+                break;
+
+            case 'pdf':
+                const fileUrl = URL.createObjectURL(e!.target!.files![0])
+                const pdf = await pdfjsLib.getDocument(fileUrl).promise
+
+                let content = ''
+                for(let i = 1; i <= pdf.numPages; i++){
+                    const page = await pdf.getPage(i)
+                    const textContent = await page.getTextContent()
+                    const pageContent = textContent.items.map(item => item.str).join(' ')
+                    content += pageContent + '\n'
+                }
+
+                const input = context?.refInput;
+                input!.current!.innerHTML = content
+                context?.setInput(content)
+
+
+                break;
+        }
+        
     }
 
     return (
